1e8a6cfa942fff68dcc845373fd81c9c3b077c43
Olivier <olivier.winkler@icloud.com>
2024-03-15 21:28:52 +0000

    feat(app): add & run prettier

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const config = {
+  singleQuote: true,
+  semi: true,
+  tabWidth: 2,
+  useTabs: false,
+  arrowParens: 'avoid',
+  trailingComma: 'es5',
+  plugins: ['prettier-plugin-organize-imports'],
+};
+
+module.exports = config;
