b79ac44ae85669904d556a52f9aec51db6e8df6d
Olivier <olivier.winkler@icloud.com>
2024-03-16 08:54:24 +0000

    feat(components): add transactions component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,60 @@
 import Actions from '@/components/actions';
+import Transactions from '@/components/transactions';
 import { Button } from '@/components/ui/button';
 import { DynamicWidget } from '@/lib/dynamic';
 
-export default function Home() {
+async function getData() {
+  const transactions = [
+    {
+      invoice: 'INV001',
+      paymentStatus: 'Paid',
+      totalAmount: '$250.00',
+      paymentMethod: 'Credit Card',
+    },
+    {
+      invoice: 'INV002',
+      paymentStatus: 'Pending',
+      totalAmount: '$150.00',
+      paymentMethod: 'PayPal',
+    },
+    {
+      invoice: 'INV003',
+      paymentStatus: 'Unpaid',
+      totalAmount: '$350.00',
+      paymentMethod: 'Bank Transfer',
+    },
+    {
+      invoice: 'INV004',
+      paymentStatus: 'Paid',
+      totalAmount: '$450.00',
+      paymentMethod: 'Credit Card',
+    },
+    {
+      invoice: 'INV005',
+      paymentStatus: 'Paid',
+      totalAmount: '$550.00',
+      paymentMethod: 'PayPal',
+    },
+    {
+      invoice: 'INV006',
+      paymentStatus: 'Pending',
+      totalAmount: '$200.00',
+      paymentMethod: 'Bank Transfer',
+    },
+    {
+      invoice: 'INV007',
+      paymentStatus: 'Unpaid',
+      totalAmount: '$300.00',
+      paymentMethod: 'Credit Card',
+    },
+  ];
+
+  return { transactions };
+}
+
+export default async function Home() {
+  const { transactions } = await getData();
+
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-between p-12 md:p-24">
       <div className="absolute top-12 right-12 md:top-24 md:right-24">
@@ -17,6 +69,7 @@ export default function Home() {
       </div>
 
       {/* <BalanaceChart /> */}
+      <Transactions transactions={transactions} />
 
       <Actions />
     </main>
---
+++ src/components/transactions.tsx
@@ -0,0 +1,50 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+
+type Props = {
+  transactions: any;
+};
+
+const Transactions = ({ transactions }: Props) => {
+  return (
+    <Table>
+      <TableCaption>A list of your recent invoices.</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">Invoice</TableHead>
+          <TableHead>Status</TableHead>
+          <TableHead>Method</TableHead>
+          <TableHead className="text-right">Amount</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {transactions.map(transaction => (
+          <TableRow key={transaction.invoice}>
+            <TableCell className="font-medium">{transaction.invoice}</TableCell>
+            <TableCell>{transaction.paymentStatus}</TableCell>
+            <TableCell>{transaction.paymentMethod}</TableCell>
+            <TableCell className="text-right">
+              {transaction.totalAmount}
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+      <TableFooter>
+        <TableRow>
+          <TableCell colSpan={3}>Total</TableCell>
+          <TableCell className="text-right">$2,500.00</TableCell>
+        </TableRow>
+      </TableFooter>
+    </Table>
+  );
+};
+
+export default Transactions;
