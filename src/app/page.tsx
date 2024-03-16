e8df90fc7879aa39ed8b70545281d0e5dd1cc68f
Olivier <olivier.winkler@icloud.com>
2024-03-16 08:07:04 +0000

    feat(home): update dynamic widget button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
 import Actions from '@/components/actions';
 import BalanaceChart from '@/components/balance-chart';
+import { Button } from '@/components/ui/button';
 import { DynamicWidget } from '@/lib/dynamic';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-12 md:p-24">
-      <DynamicWidget />
+    <main className="relative flex min-h-screen flex-col items-center justify-between p-12 md:p-24">
+      <div className="absolute top-12 right-12 md:top-24 md:right-24">
+        <DynamicWidget
+          innerButtonComponent={
+            <Button variant="outline" size="icon">
+              {/* <LogIn className='h-4 w-4' /> */}
+              Get started
+            </Button>
+          }
+        />
+      </div>
 
       <BalanaceChart />
 
