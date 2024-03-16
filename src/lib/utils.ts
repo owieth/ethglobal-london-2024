79cb5e117fcbb84c4e587596eab129191304aed4
Olivier <olivier.winkler@icloud.com>
2024-03-16 10:51:43 +0000

    feat(utils): add address shortener

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const shortenBytes32 = (bytes32: string) => {
+  return (
+    bytes32?.substring(0, 6) +
+    `...` +
+    bytes32?.substring(bytes32.length - 8, bytes32.length)
+  );
+};
